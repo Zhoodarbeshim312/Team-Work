2e598d08c89804bcfe8c5bb38d467f18d5949739
Joodarbeshim05 <162398776+Joodarbeshim05@users.noreply.github.com>
2025-11-17 23:59:24 +0600

    product

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,4 +1,10 @@
 import { Router } from "express";
+import productRoutes from "../modules/product/product.routes";
+import cors from "cors";
 
 const globalRouter = Router();
+const corsConfig = {
+  origin: ["http://localhost:3000"],
+};
+globalRouter.use("/products", cors(corsConfig), productRoutes);
 export default globalRouter;
